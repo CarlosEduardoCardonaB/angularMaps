104a0bdf3fa8463fa0d884fee6716898c1b4f0c9
Carlos Eduardo <caeca425@gmail.com>
2024-12-10 19:06:58 -0500

    se comienza implementación de standalone component con el menu de mapas

---
+++ src/app/alone/pages/alonePage/alonePage.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './alonePage.component.html',
+  styleUrl: './alonePage.component.css',
+})
+export class AlonePageComponent { }
